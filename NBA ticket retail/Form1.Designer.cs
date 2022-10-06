797aa61e4de51fd04f38c9c10fbf916384fc2a47
Henry-0810 <113506894+Henry-0810@users.noreply.github.com>
2022-10-06 11:20:43 +0100

    Add files via upload

---
+++ NBA ticket retail/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace NBA_ticket_retail
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMainMenu));
+            this.mnuMainMenu = new System.Windows.Forms.MenuStrip();
+            this.teamToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.priceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.seatsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.adminToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picMainMenu = new System.Windows.Forms.PictureBox();
+            this.yearlyRevenueAnalysisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customersReviewAnalysisToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMainMenu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnuMainMenu
+            // 
+            this.mnuMainMenu.Font = new System.Drawing.Font("Calisto MT", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mnuMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teamToolStripMenuItem,
+            this.priceToolStripMenuItem,
+            this.bookingsToolStripMenuItem,
+            this.seatsToolStripMenuItem,
+            this.adminToolStripMenuItem});
+            this.mnuMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMainMenu.Name = "mnuMainMenu";
+            this.mnuMainMenu.Size = new System.Drawing.Size(976, 27);
+            this.mnuMainMenu.TabIndex = 0;
+            this.mnuMainMenu.Text = "menuStrip1";
+            // 
+            // teamToolStripMenuItem
+            // 
+            this.teamToolStripMenuItem.Name = "teamToolStripMenuItem";
+            this.teamToolStripMenuItem.Size = new System.Drawing.Size(67, 23);
+            this.teamToolStripMenuItem.Text = "Teams";
+            // 
+            // priceToolStripMenuItem
+            // 
+            this.priceToolStripMenuItem.Name = "priceToolStripMenuItem";
+            this.priceToolStripMenuItem.Size = new System.Drawing.Size(56, 23);
+            this.priceToolStripMenuItem.Text = "Price";
+            // 
+            // bookingsToolStripMenuItem
+            // 
+            this.bookingsToolStripMenuItem.Name = "bookingsToolStripMenuItem";
+            this.bookingsToolStripMenuItem.Size = new System.Drawing.Size(85, 23);
+            this.bookingsToolStripMenuItem.Text = "Bookings";
+            // 
+            // seatsToolStripMenuItem
+            // 
+            this.seatsToolStripMenuItem.Name = "seatsToolStripMenuItem";
+            this.seatsToolStripMenuItem.Size = new System.Drawing.Size(56, 23);
+            this.seatsToolStripMenuItem.Text = "Seats";
+            // 
+            // adminToolStripMenuItem
+            // 
+            this.adminToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.adminToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yearlyRevenueAnalysisToolStripMenuItem,
+            this.customersReviewAnalysisToolStripMenuItem});
+            this.adminToolStripMenuItem.Name = "adminToolStripMenuItem";
+            this.adminToolStripMenuItem.Size = new System.Drawing.Size(70, 23);
+            this.adminToolStripMenuItem.Text = "Admin";
+            // 
+            // picMainMenu
+            // 
+            this.picMainMenu.Image = ((System.Drawing.Image)(resources.GetObject("picMainMenu.Image")));
+            this.picMainMenu.Location = new System.Drawing.Point(0, 27);
+            this.picMainMenu.Name = "picMainMenu";
+            this.picMainMenu.Size = new System.Drawing.Size(976, 541);
+            this.picMainMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMainMenu.TabIndex = 1;
+            this.picMainMenu.TabStop = false;
+            // 
+            // yearlyRevenueAnalysisToolStripMenuItem
+            // 
+            this.yearlyRevenueAnalysisToolStripMenuItem.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yearlyRevenueAnalysisToolStripMenuItem.Name = "yearlyRevenueAnalysisToolStripMenuItem";
+            this.yearlyRevenueAnalysisToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.yearlyRevenueAnalysisToolStripMenuItem.Text = "Yearly Revenue Analysis";
+            // 
+            // customersReviewAnalysisToolStripMenuItem
+            // 
+            this.customersReviewAnalysisToolStripMenuItem.Font = new System.Drawing.Font("Calisto MT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customersReviewAnalysisToolStripMenuItem.Name = "customersReviewAnalysisToolStripMenuItem";
+            this.customersReviewAnalysisToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.customersReviewAnalysisToolStripMenuItem.Text = "Customer\'s review analysis";
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(976, 568);
+            this.Controls.Add(this.picMainMenu);
+            this.Controls.Add(this.mnuMainMenu);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mnuMainMenu;
+            this.Name = "frmMainMenu";
+            this.Text = "NBA tickets retail - [Main Menu]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.mnuMainMenu.ResumeLayout(false);
+            this.mnuMainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picMainMenu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem teamToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem priceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bookingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem seatsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem adminToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem yearlyRevenueAnalysisToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem customersReviewAnalysisToolStripMenuItem;
+    }
+}
+
